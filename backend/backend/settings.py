bd7d1f36409c5f800143ea34aeafd0ca66d5a9c9
Sin-iu Ho <jb22621550@gmail.com>
2024-11-10 02:00:45 +0800

    Allow CORS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
 
     # NEW APPS
     'rest_framework',
+    'corsheaders',
     'courses',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -129,3 +131,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     'http://127.0.0.1:8000',  # Django server
+#     'http://localhost:8000',
+#     'http://127.0.0.1:51477',  # Flutter web development server
+#     'http://localhost:51477',
+# ]
